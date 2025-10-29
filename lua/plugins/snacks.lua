66b554e65a923e93b06e70443b3d91a76e971c3b
Ahmad Hasanudin <hasan13.blabla@gmail.com>
2025-10-29 10:55:10 +0700

    update snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,28 +3,34 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        find_files = {
-          grep = {
+        exclude = {
+          "vendor",
+          "node_modules",
+        },
+        grep = {},
+      },
+    },
+    keys = {
+      {
+        "<leader>ss",
+        function()
+          require("snacks.picker").grep({
             args = {
-              "--glob=!vendor/**",
-              "--glob=!node_modules/**",
-              "--glob=!**/vendor/**",
-              "--glob=!**/node_modules/**",
+              "--vimgrep",
+              "--smart-case",
+              "--hidden",
+              "--glob",
+              "!**/vendor/**",
+              "--glob",
+              "!vendor/**",
+              "--glob",
+              "!**node_modules/**",
+              "--glob",
+              "!/Users/ahmadhasanudin/projects/old_app/**",
             },
-          },
-        },
-        grep = {
-          args = {
-            "rg",
-            "--vimgrep",
-            "--smart-case",
-            "--hidden",
-            "--glob=!**/vendor/**",
-            "--glob=!vendor/**",
-            "--glob=!**node_modules/**",
-            "--glob=!/Users/ahmadhasanudin/projects/old_app/**",
-          },
-        },
+          })
+        end,
+        desc = "Grep with exclusions",
       },
     },
   },
